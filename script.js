02c1ad3ed3206ec6acad0d210a81e76a5b677450
Ashymaliev <nursultan-99kg@mail.ru>
2021-11-24 16:41:47 +0600

    task 1

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-const map = new Map([
-    [1, 'I'],
-    [5, 'V'],
-    [10, 'X'],
-    [50, 'L'],
-    [100, 'C'],
-    [500, 'D'],
-    [1000, 'M']
-  ])
-  
-  /**
-   * @param {number} num
-   * @return {string}
-   */
-
-  var intToRoman = function(num) {
-    let base = 1
-    const result = []
-    while (num > 0) {
-      // get last digit
-      const last = num % 10
-      
-      if (last < 4) {
-        for (let k = last; k > 0; k--) {
-          result.unshift(map.get(base))
-        }
-      } else if (last === 4) {
-        result.unshift(...[map.get(base), map.get(base * 5)])
-      } else if (last === 5) {
-        result.unshift(map.get(base * 5))
-      } else if (last < 9) {
-        for (let k = last; k > 5; k--) {
-          result.unshift(map.get(base))
-        }
-        result.unshift(map.get(base * 5))
-      } else {
-        result.unshift(...[map.get(base), map.get(base * 10)])
-      }
-      
-      base *= 10
-      
-      num = (num - last) / 10
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+ var letterCombinations = function(digits) {
+    
+    let letters = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r','s'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y','z'],
     }
-    return result.join('')
-  };
+    
+    let num = [];
+    num = [" "];
+    
+    if(!digits)return [];
+    
+    for(let i=0; i < digits.length; i++){
+        let digit = digits[i]
+        
+        let n = num.length;
+        
+        for(let j=0; j < n; j++){
+            
+            let lookUp = letters[digit];
+            let pop = num.shift();
+            for(let k =0; k < lookUp.length; k++){
+                
+                let newData = pop + lookUp[k]
+                num.push(newData.trim())
+            }
+        }
+    }
+    return num
+};
